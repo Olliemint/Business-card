cdf70eb851d9e28326e387a2f55501f35364c5db
Olliemint <furywrld5@gmail.com>
2022-08-07 06:27:48 +0300

    feat:added components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Info from './Components/Info';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="container">
+        <div className="content-section">
+          <Info />
+
+        </div>
+
+      </section>
     </div>
   );
 }
---
+++ src/Components/Info.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./style.css"
+
+const Info = () => {
+  return (
+      <section className="info">
+          <h3>HELLO</h3>
+          <div className="image">
+              <img src="" alt=""  />
+          </div>
+    </section>
+  )
+}
+
+export default Info
